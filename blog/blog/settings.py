442ed1b896f3011f967f9bb6dac3ffe9001efe08
oldmanleo <slipplay97@gmail.com>
2023-06-07 06:00:07 +0300

    make registration page

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'blog_posts.apps.BlogPostsConfig',
+    'users',
 ]
 
 MIDDLEWARE = [
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_posts.urls')),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', include('users.urls'))
+
 ]
 
 if settings.DEBUG:
