cb86b66df50ee06a6aa4426b1cde28d1bc76d85a
oldmanleo <slipplay97@gmail.com>
2023-06-11 01:35:23 +0300

    add tags to blog posts

--- blog/blog_posts/models.py
+++ blog/blog_posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -8,6 +9,7 @@ class Post(models.Model):
     post_date = models.DateField('Дата создания поста', auto_now_add=True)
     img = models.ImageField('Изображение', upload_to='image/%Y', default='image/default.jpg')
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return f'{self.title}, {self.author}'
--- blog/blog_posts/serializers.py
+++ blog/blog_posts/serializers.py
@@ -1,14 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Post
+from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    tags = TagListSerializerField()
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'description', 'owner']
+        fields = ['id', 'title', 'description', 'tags', 'owner', 'post_date', 'img', 'author']
+        read_only_fields = ['post_date']
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        owner_username = validated_data.pop('owner')
+        owner = User.objects.get(username=owner_username)
+        post = Post.objects.create(owner=owner, **validated_data)
+        for tag in tags_data:
+            post.tags.add(tag)
+        return post
+
+    def update(self, instance, validated_data):
+        tags_data = validated_data.pop('tags')
+        instance = super().update(instance, validated_data)
+        instance.tags.set(tags_data)
+        return instance
 
 
 class UserSerializer(serializers.ModelSerializer):
